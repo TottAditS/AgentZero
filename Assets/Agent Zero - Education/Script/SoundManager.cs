78f30355d3c998a975032d1b05bf955918585665
Aaronmedhavi <aarondroid04@gmail.com>
2024-12-14 23:18:31 +0700

    Update narration + sound manager

--- Assets/Agent Zero - Education/Script/SoundManager.cs
+++ Assets/Agent Zero - Education/Script/SoundManager.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 using UnityEngine.Audio;
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance;
+    public static SoundManager Instance;
     public AudioMixer audioMixer;
     public string masterVolumeParameter = "MasterVolume";
+    public AudioClip mainMenuBGM;
+    private AudioSource bgmSource;
 
     private void Awake()
     {
-        if (instance == null)
+        if (Instance == null)
         {
-            instance = this;
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            bgmSource = gameObject.AddComponent<AudioSource>();
+            bgmSource.loop = true;
+            bgmSource.playOnAwake = false;  
+            bgmSource.clip = mainMenuBGM;
+            bgmSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups("Master")[0];
         }
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (!bgmSource.isPlaying)
+        {
+            bgmSource.Play();
         }
     }
 
@@ -25,4 +42,18 @@ public class SoundManager : MonoBehaviour
         float mixerVolume = Mathf.Log10(volume) * 20;
         audioMixer.SetFloat(masterVolumeParameter, mixerVolume);
     }
+
+    public void StopBGM()
+    {
+        bgmSource.Stop();
+    }
+
+    public void StartBGM()
+    {
+        if (bgmSource.clip != null && !bgmSource.isPlaying)
+        {
+            bgmSource.Play();
+        }
+    }
+
 }
